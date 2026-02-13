98a661ed7b02732d1fef57a1884ee7fea23df82f
arinovalua-sketch <arinovalua@gmail.com>
2026-02-13 17:45:20 +0500

    Set DEBUG False

--- lost_and_found/settings.py
+++ lost_and_found/settings.py
@@ -10,9 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+DEBUG = False
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
